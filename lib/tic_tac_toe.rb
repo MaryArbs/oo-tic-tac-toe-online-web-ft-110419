f1a483b2260f7474a4e4d23bf952b01c43462c18
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-11-15 18:47:11 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -60,8 +60,17 @@ turn_count% 2 == 0 ? "X" : "O"
 end
 
 def turn
-  turn_count.gets
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  if valid_move?(index)
+    move(index, current_player)
+    display_board
+  else
+    turn
+  end
 end
+
 
 
 end
